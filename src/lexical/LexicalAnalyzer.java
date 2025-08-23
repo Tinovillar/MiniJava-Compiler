6dddd6ee0af4735b3889cbcfdfeddb804d48d0e7
Valentino <valennvillar@gmail.com>
2025-08-23 18:00:00 -0300

    To implement

--- src/lexical/LexicalAnalyzer.java
+++ src/lexical/LexicalAnalyzer.java
@@ -31,4 +31,27 @@ public class LexicalAnalyzer {
     }
 
     private Token e0() {return null;}
+
+    private Token e1Number() {return null;}
+    private Token e1Word() {return null;}
+    private Token e1SimpleComment() {return null;}
+    private Token e1MultipleComment() {return null;}
+    private Token e1Blank() {return null;}
+    private Token e1Equal() {return null;}
+    private Token e1NonEqual() {return null;}
+    private Token e1Lower() {return null;}
+    private Token e1Higher() {return null;}
+    private Token e1Add() {return null;}
+    private Token e1Sub() {return null;}
+    private Token e1Mul() {return null;}
+    private Token e1Div() {return null;}
+    private Token e1And() {return null;}
+    private Token e1Or() {return null;}
+    private Token e1Parenthesis() {return null;}
+    private Token e1Bracket1() {return null;}
+    private Token e1Bracket2() {return null;}
+    private Token e1Dot() {return null;}
+    private Token e1Comma() {return null;}
+    private Token e1Colon() {return null;}
+    private Token e1SemiColon() {return null;}
 }
