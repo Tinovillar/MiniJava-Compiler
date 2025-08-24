d01c6249dc0c9fbab98e5ddb1e40dc75d8c6d686
Valentino <valennvillar@gmail.com>
2025-08-24 20:19:14 -0300

    implementing e0 with some e1 states too

--- src/lexical/LexicalAnalyzer.java
+++ src/lexical/LexicalAnalyzer.java
@@ -3,6 +3,7 @@ package lexical;
 import sourceManager.SourceManager;
 
 import java.io.IOException;
+import java.lang.Character;
 
 public class LexicalAnalyzer {
     String lexeme;
@@ -29,22 +30,98 @@ public class LexicalAnalyzer {
             e.printStackTrace();
         }
     }
+    private void updateLexemeAndCurrentChar() {
+        updateLexeme();
+        updateCurrentChar();
+    }
 
-    private Token e0() {return null;}
+    private Token e0() {
+        updateCurrentChar();
 
-    private Token e1Number() {return null;}
-    private Token e1Word() {return null;}
-    private Token e1SimpleComment() {return null;}
-    private Token e1MultipleComment() {return null;}
-    private Token e1Blank() {return null;}
+        switch(currentChar) {
+            // BLANK
+            case ' ':
+            case '\n':
+            case '\t':
+                e0();
+                break;
+            // COMMENTS OR DIVISION
+            case '/':
+            // OPERATORS
+            case '=':
+                updateLexemeAndCurrentChar();
+                return e1Equal();
+            case '!':
+                updateLexemeAndCurrentChar();
+                return e1ExclamationMark();
+            case '<':
+            case '>':
+            default:
+                if(Character.isLetter(currentChar)) {
+                    updateLexemeAndCurrentChar();
+                    return e1Letter();
+                } else if(Character.isDigit(currentChar)) {
+                    updateLexemeAndCurrentChar();
+                    return e1Digit();
+                }
+        }
+        return null;
+    }
+
+    private Token e1Digit() {
+        if(Character.isDigit(currentChar)) {
+            updateLexemeAndCurrentChar();
+            return e1Digit();
+        } else {
+            return new Token(0, lexeme, sourceManager.getLineNumber());
+        }
+    }
+    private Token e1Letter() {return null;}
+    private Token e1SimpleComment() {
+        while(currentChar != '\n') {
+            updateCurrentChar();
+        }
+        lexeme = "";
+        return e0();
+    }
+    private Token e1MultipleComment() {
+        while(true) {
+            if(currentChar == '*') {
+                updateCurrentChar();
+                if(currentChar == '/')
+                    break;
+            } else {
+                updateCurrentChar();
+            }
+        }
+        lexeme = "";
+        return e0();
+    }
     private Token e1Equal() {return null;}
     private Token e1NonEqual() {return null;}
+    private Token e1ExclamationMark() {
+        if(currentChar == '=') {
+            updateLexemeAndCurrentChar();
+            return new Token(0, lexeme, sourceManager.getLineNumber());
+        }
+        return null; // Exception
+    }
     private Token e1Lower() {return null;}
     private Token e1Higher() {return null;}
     private Token e1Add() {return null;}
     private Token e1Sub() {return null;}
     private Token e1Mul() {return null;}
-    private Token e1Div() {return null;}
+    private Token e1Slash() {
+        if(currentChar == '/') {
+            updateCurrentChar();
+            return e1SimpleComment();
+        } else if(currentChar == '*') {
+            updateCurrentChar();
+            return e1MultipleComment();
+        } else {
+            return new Token(0, lexeme, sourceManager.getLineNumber());
+        }
+    }
     private Token e1And() {return null;}
     private Token e1Or() {return null;}
     private Token e1Parenthesis() {return null;}
