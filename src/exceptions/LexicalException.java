917bddbe4429a51ab23d37f98b6954feb4dedd23
Valentino <valennvillar@gmail.com>
2025-08-27 19:31:26 -0300

    LexicalException improved

--- src/exceptions/LexicalException.java
+++ src/exceptions/LexicalException.java
@@ -1,18 +1,23 @@
 package exceptions;
 
 public class LexicalException extends Exception {
-    private String lexeme;
+    private String currentLine;
     private int line;
     private int column;
 
-    public LexicalException(String lexeme, int line, int column) {
-        this.lexeme = lexeme;
+    public LexicalException(String currentLine, int line, int column) {
+        this.currentLine = currentLine;
         this.line = line;
         this.column = column;
     }
 
     @Override
     public void printStackTrace() {
-        System.out.println("Lexeme: " + lexeme + " - Line number: " + line);
+        System.out.println("Error ==> Lexeme: \"" + currentLine + "\" - Line number: " + line);
+        for (int i = 0; i < 18 + column; i++) {
+            System.out.print(" ");
+        }
+        System.out.print("^");
+        System.out.println();
     }
 }
