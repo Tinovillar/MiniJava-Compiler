dcafb8b37af19cbf70e565486f6f2683e2bd068e
Valentino <valennvillar@gmail.com>
2025-08-27 19:38:49 -0300

    Main implemented

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import exceptions.LexicalException;
+import lexical.ID;
+import lexical.LexicalAnalyzer;
+import lexical.Token;
+import sourceManager.BetterSourceManagerImpl;
+import sourceManager.SourceManager;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            SourceManager sourceManager = new BetterSourceManagerImpl();
+            sourceManager.open("C:\\Users\\valen\\Desktop\\compilador\\compilador\\resource\\test1.java");
 
+            Token currentToken = null;
+            LexicalAnalyzer lexicalAnalyzer = new LexicalAnalyzer(sourceManager);
+
+            do {
+                try {
+                    currentToken = lexicalAnalyzer.getNextToken();
+                    System.out.println(currentToken.toString());
+                } catch (LexicalException e) {
+                    e.printStackTrace();
+                }
+            } while(currentToken.getId() != ID.EOF);
+
+            sourceManager.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
