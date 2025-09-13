4dfbe9d77a96fbd40f21a6a84c4d5efca388a790
Valentino <valennvillar@gmail.com>
2025-09-13 12:26:56 -0300

    Primeros (not ready) and better grammar

---
+++ src/syntactic/Primeros.java
@@ -0,0 +1,85 @@
+package syntactic;
+
+import java.util.*;
+
+public class Primeros {
+    private static final Map<String, Set<String>> primeros = new HashMap<>();
+    static {
+        // Terminales simples
+        primeros.put("MetodoEncadenado", Set.of("."));
+        primeros.put("VarEncadenada", Set.of("."));
+        primeros.put("ListaExpsResto", Set.of(",", "€"));
+        primeros.put("ArgsActuales", Set.of("("));
+        primeros.put("ArgsFormales", Set.of("("));
+        primeros.put("ExpresionParentizada", Set.of("("));
+        primeros.put("LlamadaConstructor", Set.of("new"));
+        primeros.put("LlamadaMetodoEstatico", Set.of("idClase"));
+        primeros.put("LlamadaMetodo", Set.of("idMetVar"));
+        primeros.put("AccesoVar", Set.of("idMetVar"));
+        primeros.put("VarLocal", Set.of("var"));
+        primeros.put("Return", Set.of("return"));
+        primeros.put("If", Set.of("if"));
+        primeros.put("Else", Set.of("else"));
+        primeros.put("While", Set.of("while"));
+        primeros.put("Bloque", Set.of("{"));
+        primeros.put("BloqueOpcional", Set.of("{", "€"));
+        primeros.put("OperadorAsignacion", Set.of("="));
+        primeros.put("OperadorUnario", Set.of("+", "-", "++", "--", "!"));
+        primeros.put("OperadorBinario", Set.of("||", "&&", "==", "!=", "<", ">", "<=", ">=", "+", "-", "*", "/", "%"));
+        primeros.put("Primitivo", Set.of("true", "false", "intLiteral", "charLiteral", "null"));
+        primeros.put("ModificadorMiembro", Set.of("abstract", "static", "final"));
+        primeros.put("ModificadorOpcional", Set.of("abstract", "static", "final", "€")); // Usar el de arriba.
+        primeros.put("HerenciaOpcional", Set.of("extends", "€"));
+        primeros.put("DeclaracionVariable", Set.of("=", "€"));
+        primeros.put("TipoPrimitivo", Set.of("boolean", "char", "int"));
+        primeros.put("Tipo", union(Set.of("idClase"), primeros.get("TipoPrimitivo")));
+        primeros.put("TipoMetodo", union(Set.of("void"), primeros.get("Tipo")));
+        primeros.put("ArgFormal", primeros.get("Tipo"));
+        primeros.put("ListaArgsFormales", primeros.get("ArgFormal"));
+        primeros.put("ListaArgsFormalesOpcional", union(Set.of("€"), primeros.get("ListaArgsFormales")));
+        primeros.put("ListaArgsFormalesResto", Set.of(",", "€"));
+        primeros.put("Constructor", Set.of("public"));
+        primeros.put("Clase", union(Set.of("class"), primeros.get("ModificadorOpcional")));
+        primeros.put("ListaClases", union(Set.of("€"), primeros.get("Clase")));
+        primeros.put("Inicial", union(Set.of("EOF"), primeros.get("Clase")));
+        primeros.put("Primario", union(
+                Set.of("this", "stringLiteral"),
+                primeros.get("AccesoVar"),
+                primeros.get("LlamadaConstructor"),
+                primeros.get("LlamadaMetodo"),
+                primeros.get("LlamadaMetodoEstatico"),
+                primeros.get("ExpresionParentizada")
+        ));
+        primeros.put("Referencia", primeros.get("Primario"));
+        primeros.put("Operando", union(
+                primeros.get("Primitivo"),
+                primeros.get("Referencia")
+        ));
+
+        // Expresión compuesta
+        primeros.put("ExpresionCompuesta", primeros.get("ExpresionBasica"));
+
+        // Expresión
+        primeros.put("Expresion", primeros.get("ExpresionCompuesta"));
+
+        // Miembro
+        primeros.put("Miembro", Set.of("void", "abstract", "static", "final", "public", "boolean", "char", "int", "idClase"));
+
+        // Mostrar
+        for (var entry : primeros.entrySet()) {
+            System.out.println(entry.getKey() + " → " + entry.getValue());
+        }
+    }
+
+    @SafeVarargs
+    private static <T> Set<T> union(Set<T>... sets) {
+        Set<T> result = new HashSet<>();
+        for (Set<T> s : sets) {
+            if (s != null) {
+                result.addAll(s);
+            }
+        }
+        return result;
+    }
+}
+
