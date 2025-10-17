16fd3be54a98b1f84b034a54375d888baa49e9f0
Valentino <valennvillar@gmail.com>
2025-10-17 19:26:58 -0300

    emptySentenceNode

--- src/semantic/nodes/sentence/EmptySentenceNode.java
+++ src/semantic/nodes/sentence/EmptySentenceNode.java
@@ -3,5 +3,7 @@ package semantic.nodes.sentence;
 import exceptions.SemanticException;
 
 public class EmptySentenceNode extends SentenceNode {
-    public void check() throws SemanticException {}
+    public void check() throws SemanticException {
+        checked = true;
+    }
 }
