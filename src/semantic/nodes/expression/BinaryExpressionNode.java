d8f114ec57f72b977d6d1120123140e2079214a8
Valentino <valennvillar@gmail.com>
2025-10-25 20:33:24 -0300

    feat(BinaryExpressionNode): check method implemented using the changes of the last commit

--- src/semantic/nodes/expression/BinaryExpressionNode.java
+++ src/semantic/nodes/expression/BinaryExpressionNode.java
@@ -2,6 +2,9 @@ package semantic.nodes.expression;
 
 import exceptions.SemanticException;
 import lexical.Token;
+import lexical.lexID;
+import semantic.nodes.literal.FactoryNode;
+import semantic.type.PrimitiveType;
 import semantic.type.Type;
 
 public class BinaryExpressionNode extends ExpressionNode {
@@ -10,8 +13,42 @@ public class BinaryExpressionNode extends ExpressionNode {
     private Token operator;
 
     public Type check() throws SemanticException {
-        return null;
+        Type leftType = leftExpression.check();
+        Type rightType = rightExpression.check();
+        Type resultType = getResultType();
+
+        switch (operator.getId()) {
+            // Operadores aritméticos: +, -, *, /, %
+            case op_plus, op_minus, op_multiplication, op_division, op_mod -> {
+                if (!leftType.equals("int") || !rightType.equals("int")) {
+                    // TODO exception
+                }
+            }
+            // Operadores booleanos: &&, ||
+            case op_and, op_or -> {
+                if (!leftType.isBoolean() || !rightType.isBoolean()) {
+                    // TODO exception
+                }
+            }
+            // Operadores relacionales: <, <=, >, >=
+            case op_greater_than, op_greater_than_equal, op_less_than, op_less_than_equal -> {
+                if (!leftType.equals("int") || !rightType.equals("int")) {
+                    // TODO exception
+                }
+            }
+            // Operadores de igualdad: ==, !=
+            case op_equal_equal, op_not_equal -> {
+                if (!leftType.conformsTo(rightType) && !rightType.conformsTo(leftType)) {
+                    // TODO exception
+                }
+            }
+            default -> {
+                // TODO exception
+            }
+        }
+        return resultType;
     }
+
     public ExpressionNode getLeftExpression() {
         return leftExpression;
     }
@@ -29,5 +66,20 @@ public class BinaryExpressionNode extends ExpressionNode {
     }
     public void setOperator(Token operator) {
         this.operator = operator;
+    }
+    private Type getResultType() {
+        Type toReturn;
+        switch (operator.getId()) {
+            case op_minus, op_plus, op_division, op_multiplication, op_mod -> {
+                toReturn = new PrimitiveType(new Token(lexID.kw_int, "int", -1));
+            }
+            case op_and, op_or,op_greater_than,op_greater_than_equal,op_less_than,op_less_than_equal,op_equal_equal,op_not_equal -> {
+                toReturn = new PrimitiveType(new Token(lexID.kw_boolean, "boolean", -1));
+            }
+            default -> {
+                toReturn = null;
+            }
+        }
+        return toReturn;
     }
 }
