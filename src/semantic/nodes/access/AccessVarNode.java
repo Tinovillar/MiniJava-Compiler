05116f2d81d6315d0f40b46596eef09152ba4eee
Valentino <valennvillar@gmail.com>
2025-10-25 17:44:23 -0300

    accessVarNode check method ok

--- src/semantic/nodes/access/AccessVarNode.java
+++ src/semantic/nodes/access/AccessVarNode.java
@@ -1,7 +1,11 @@
 package semantic.nodes.access;
 
+import compiler.Main;
 import exceptions.SemanticException;
 import lexical.Token;
+import semantic.model.Attribute;
+import semantic.model.Parameter;
+import semantic.nodes.sentence.LocalVarNode;
 import semantic.type.Type;
 
 public class AccessVarNode extends AccessNode {
@@ -12,6 +16,22 @@ public class AccessVarNode extends AccessNode {
     }
 
     public Type check() throws SemanticException {
-        return null;
+        Type type;
+        Attribute attribute = Main.ST.getCurrentClass().getAttributes().get(id.getLexeme());
+        Parameter parameter = Main.ST.getCurrentMethod().getParameters().get(id.getLexeme());
+        LocalVarNode localVarNode = Main.ST.getCurrentBlock().getLocalVarMap().get(id.getLexeme());
+        if(attribute != null) {
+            type = attribute.getType();
+        } else if(parameter != null) {
+            type = parameter.getType();
+        } else if(localVarNode != null) {
+            type = localVarNode.getType();
+        } else {
+            throw new SemanticException(id, "Variable no declarada"); // TODO exception mejorarla
+        }
+        if(chained != null) {
+            type = chained.check(type);
+        }
+        return type;
     }
 }
