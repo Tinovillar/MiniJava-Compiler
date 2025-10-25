2f62cd4821f44a33f3ff372e70d232baffdac0f8
Valentino <valennvillar@gmail.com>
2025-10-25 17:57:14 -0300

    staticMethod with check implemented

--- src/semantic/nodes/access/StaticMethodCallNode.java
+++ src/semantic/nodes/access/StaticMethodCallNode.java
@@ -1,7 +1,11 @@
 package semantic.nodes.access;
 
+import compiler.Main;
 import exceptions.SemanticException;
 import lexical.Token;
+import semantic.model.ConcreteClass;
+import semantic.model.Method;
+import semantic.model.Parameter;
 import semantic.nodes.expression.ExpressionNode;
 import semantic.type.Type;
 
@@ -19,6 +23,26 @@ public class StaticMethodCallNode extends AccessNode {
     }
 
     public Type check() throws SemanticException {
-        return null;
+        ConcreteClass class_ = Main.ST.getClassOrNull(idClass.getLexeme());
+        if(class_ == null) {
+            // TODO exception la clase no existe
+        }
+        Method method = class_.getMethods().get(idMetOrVar.getLexeme());
+        if(method == null) {
+            // TODO exception el metodo no existe en la clase
+        }
+        if(!method.getModifier().getLexeme().equals("static")) {
+            // TODO exception el metodo no es estatico
+        }
+        List<Parameter> params = method.getParameters().values().stream().toList();
+        int index = 0;
+        for(ExpressionNode arg : args) {
+            Type type = arg.check();
+            if(Main.ST.isSubtypeOf(type.getName(), params.get(index).getType().getName())) {
+                // TODO exception no coincide el tipo de los parametros
+            }
+            index++;
+        }
+        return method.getReturnType();
     }
 }
