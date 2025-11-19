7c8b5e3f5f1a9786beabd605c9c79279f85eb4d1
Valentino <valennvillar@gmail.com>
2025-11-19 14:08:06 -0300

    Main

--- src/compiler/Main.java
+++ src/compiler/Main.java
@@ -6,18 +6,20 @@ import exceptions.SyntacticException;
 import lexical.LexicalAnalyzer;
 import semantic.SymbolTable;
 import sourceManager.BetterSourceManagerImpl;
+import sourceManager.OutputWriter;
 import sourceManager.SourceManager;
 import syntactic.SyntacticAnalyzer;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.PathMatcher;
 
 public class Main {
     public static SymbolTable ST;
     public static void main(String[] args) {
         try {
-            boolean errors = false;
-
             if(args.length != 1) {
                 System.out.println("ERROR: Need ONE and only ONE argument");
                 return;
@@ -40,9 +42,14 @@ public class Main {
 
             ST.generate();
 
+            String outputName = getFileName(args[0]);
+
+            OutputWriter.createFile("["+ outputName +"]", ST.getInstructions());
+
             System.out.println("[SinErrores]");
 
-            ST.printTable();
+//            ST.printTable();
+//            ST.printInstructions();
 
             sourceManager.close();
         } catch (FileNotFoundException e) {
@@ -54,5 +61,8 @@ public class Main {
         } catch (SemanticException e) {
             e.printStackTrace();
         }
+    }
+    public static String getFileName(String arg) {
+        return arg.substring(Math.max(arg.lastIndexOf('/'), arg.lastIndexOf('\\')) + 1);
     }
 }
